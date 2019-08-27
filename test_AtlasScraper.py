08bbfc89c9029af3888cf1a9e8a3c6c132526249
Nehal Patel <nehal26@gmail.com>
2019-08-27 21:28:29 +0100

    First commit, attempting test driven development. Have included test

---
+++ test_AtlasScraper.py
@@ -0,0 +1,14 @@
+import AtlasScraper
+
+def test_read_in_file():
+
+    output = AtlasScraper.read_in_file('test_data.csv')
+
+    # Check that output is a list
+    assert isinstance(output, list)
+
+    # Check that contents of list are all strings
+    assert all(isinstance(item, str) for item in output)
+
+    # Check that all gene_names start with FBgn
+    assert all([item[:4] == 'FBgn' for item in output])
