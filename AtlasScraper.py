610d2947115d90c18fd011197f12a1ff56c5fbfd
Nehal Patel <nehal26@gmail.com>
2019-08-31 18:57:02 +0100

    Added a function which takes list as input and returns dictionary with key as gene name and value as gene info.

--- AtlasScraper.py
+++ AtlasScraper.py
@@ -1,17 +1,44 @@
 import requests
 import csv
+from time import sleep
+
 
 def scrape_fly_atlas2():
     pass
 
-def read_in_file(gene_list: str)->list:
+
+def read_in_file(gene_list: str) -> list:
+    # Function reads in a csv file and outputs a gene list
     with open(gene_list) as csv_file:
         reader = csv.reader(csv_file)
-        gene_name_list = [row[0] for row in reader if row[0][:4]=='FBgn']
+        gene_name_list = [row[0] for row in reader if row[0][:4] == "FBgn"]
         return gene_name_list
 
-def get_gene_data(gene_name: str)->str:
-    url = f"http://flyatlas.gla.ac.uk/FA2Direct/index.html?fbgn={gene_name}&tableOut=gene"
+
+def get_gene_data(gene_name: str) -> str:
+    # Function accepts a gene name and requests the information rom flyatlas. Returns the text.
+    url = (
+        f"http://flyatlas.gla.ac.uk/FA2Direct/index.html?fbgn={gene_name}&tableOut=gene"
+    )
     r = requests.get(url)
     r.raise_for_status()
     return r.text
+
+
+def request_all_gene_data(gene_name_list: list) -> dict:
+    # Function that accepts a cleaned up name of gene lists and returns the gene data as a dict
+    # with gene name as key and gene data as value
+    gene_data_dict = {}
+    number_of_genes = len(gene_name_list)
+    for idx, gene_name in enumerate(gene_name_list, 1):
+        gene_data = get_gene_data(gene_name)
+        sleep(1)
+        gene_data_dict[gene_name] = gene_data
+        print(
+            f"{idx} of {number_of_genes} requested. {round(idx/number_of_genes*100,1)}% complete."
+        )
+    return gene_data_dict
+
+
+def generate_output_text_file(gene_data_dict: dict, output_file_name: str):
+    pass
--- test_AtlasScraper.py
+++ test_AtlasScraper.py
@@ -1,7 +1,8 @@
 import AtlasScraper as AttScr
 
+
 def test_read_in_file():
-    output = AttScr.read_in_file('test_data/test_data_1_file_reading.csv')
+    output = AttScr.read_in_file("test_data/test_data_1_file_reading.csv")
 
     # Check that output is a list
     assert isinstance(output, list)
@@ -10,10 +11,23 @@ def test_read_in_file():
     assert all(isinstance(item, str) for item in output)
 
     # Check that all gene_names start with FBgn
-    assert all([item[:4] == 'FBgn' for item in output])
+    assert all([item[:4] == "FBgn" for item in output])
+
 
 def test_get_gene_data():
     # Check that output is a list
-    assert isinstance(AttScr.get_gene_data('FBgn0030313'), str)
-    assert isinstance(AttScr.get_gene_data('hello'), str)
-    assert isinstance(AttScr.get_gene_data('1234'), str)
+    assert isinstance(AttScr.get_gene_data("FBgn0030313"), str)
+    assert isinstance(AttScr.get_gene_data("hello"), str)
+    assert isinstance(AttScr.get_gene_data("1234"), str)
+
+    # Check if bad input results in "An error has occurred."
+    assert AttScr.get_gene_data("Random thing1") == "An error has occurred.\r\n"
+    assert AttScr.get_gene_data("123413") == "An error has occurred.\r\n"
+    assert AttScr.get_gene_data("xnnaiunas_wdsnak") == "An error has occurred.\r\n"
+
+
+def test_request_all_gene_data():
+    gene_list = AttScr.read_in_file("test_data/test_data_2_website_response.csv")
+
+    # Check that output is a dictionary
+    assert isinstance(AttScr.request_all_gene_data(gene_list), dict)
