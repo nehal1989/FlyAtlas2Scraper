37cefc5f54023c6b9864bbc9bde282ea6a071ccb
Nehal Patel <nehal26@gmail.com>
2019-10-01 22:09:59 +0100

    Broke up test into smaller test

--- test_AtlasScraper.py
+++ test_AtlasScraper.py
@@ -1,33 +1,38 @@
 import AtlasScraper as AttScr
+import pytest
 
+@pytest.fixture
+def test_data_1():
+    return AttScr.read_in_file("test_data/test_data_1_file_reading.csv")
 
-def test_read_in_file():
-    output = AttScr.read_in_file("test_data/test_data_1_file_reading.csv")
+@pytest.fixture
+def test_data_2():
+    return AttScr.read_in_file("test_data/test_data_2_website_response.csv")
 
-    # Check that output is a list
-    assert isinstance(output, list)
+def test_read_in_file_is_list(test_data_1):
+    assert isinstance(test_data_1, list)
 
-    # Check that contents of list are all strings
-    assert all(isinstance(item, str) for item in output)
+def test_read_in_file_list_contents_are_string(test_data_1):
+    assert all(isinstance(item, str) for item in test_data_1)
 
-    # Check that all gene_names start with FBgn
-    assert all([item[:4] == "FBgn" for item in output])
+def test_read_in_file_list_contents_are_begin_with_FBgn(test_data_1):
+    assert all([item[:4] == "FBgn" for item in test_data_1])
 
-
-def test_get_gene_data():
-    # Check that output is a list
+def test_get_gene_data_return_is_string():
     assert isinstance(AttScr.get_gene_data("FBgn0030313"), str)
     assert isinstance(AttScr.get_gene_data("hello"), str)
     assert isinstance(AttScr.get_gene_data("1234"), str)
 
-    # Check if bad input results in "An error has occurred."
+def test_get_gene_data_has_correct_error():
     assert AttScr.get_gene_data("Random thing1") == "An error has occurred.\r\n"
     assert AttScr.get_gene_data("123413") == "An error has occurred.\r\n"
     assert AttScr.get_gene_data("xnnaiunas_wdsnak") == "An error has occurred.\r\n"
 
 
-def test_request_all_gene_data():
-    gene_list = AttScr.read_in_file("test_data/test_data_2_website_response.csv")
 
+
+
+
+def test_request_all_gene_data(test_data_2):
     # Check that output is a dictionary
-    assert isinstance(AttScr.request_all_gene_data(gene_list), dict)
+    assert isinstance(AttScr.request_all_gene_data(test_data_2), dict)
